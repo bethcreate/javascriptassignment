6aaf5fb2d2e872261b87c2a2cf36dac53d40df41
bethcreate <kamaubetth@gmail.com>
2021-03-22 07:42:33 +0300

    first commit

---
+++ statements.js
@@ -0,0 +1,23 @@
+function getGrade (marks){
+    var marks=0;
+    if (marks>=80){
+        console.log("A");
+    }
+    else if(marks>=70){
+        console.log("B");
+    }
+    else if(marks>=60){
+        console.log("C");
+    }
+    else if(marks>=50){
+        console.log("D");
+    }
+    else if(marks>=0){
+        console.log("E");
+    }
+    
+    }
+    
+    
+    getGrade();
+    
